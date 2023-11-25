8ed2f53a32e4f58ccff5fbf86cbd00b962af0e71
klodmit <klochixindima8@gmail.com>
2023-11-25 14:20:58 +0300

    new changes

--- app/src/main/java/ru/klodmit/notesappmvvm/MainActivity.kt
+++ app/src/main/java/ru/klodmit/notesappmvvm/MainActivity.kt
@@ -3,44 +3,66 @@ package ru.klodmit.notesappmvvm
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.rememberTopAppBarState
+import androidx.compose.material3.surfaceColorAtElevation
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import ru.klodmit.notesappmvvm.navigation.NotesNavHost
 import ru.klodmit.notesappmvvm.ui.theme.NotesAppMVVMTheme
 
 class MainActivity : ComponentActivity() {
+    @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             NotesAppMVVMTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
+                Scaffold(
+                    topBar = {
+                        TopAppBar(
+                            title = {
+                                Text(text = "Notes App")
+                            },
+                            Modifier
+                                .shadow(
+                                    elevation = 12.dp
+                                )
+                        )
+                    },
+                    content = { padding ->
+                        Surface(
+                            modifier = Modifier
+                                .padding(padding)
+                                .fillMaxSize(),
+                            color = MaterialTheme.colorScheme.background
+                        ) {
+                            NotesNavHost()
+                        }
+                    }
+
+                )
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     NotesAppMVVMTheme {
-        Greeting("Android")
+
     }
 }
---
+++ app/src/main/java/ru/klodmit/notesappmvvm/screens/Start.kt
@@ -0,0 +1,11 @@
+package ru.klodmit.notesappmvvm.screens
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavHostController
+
+
+@Composable
+fun Start(navController: NavHostController) {
+    Text(text = "Notes App Screen")
+}
